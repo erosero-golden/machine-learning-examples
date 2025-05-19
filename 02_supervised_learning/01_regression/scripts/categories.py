b2fbbabdd0dd7b94be96aa5cd0ae9ced9e341e42
Luis Eduardo Rosero Correa <>
2025-05-19 17:49:47 -0500

    :sparkles: chore(scripts): adding script to manage data transformation

---
+++ 02_supervised_learning/01_regression/scripts/categories.py
@@ -0,0 +1,9 @@
+categories = [
+    "Action", "Comedy", "Drama", "Documentary", "Kids", "Horror", "Sci-Fi", "Romance", "Western"
+]
+
+categories_map = {}
+for i, category in enumerate(categories):
+    categories_map[category] = i
+
+print(categories_map)
